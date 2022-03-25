a455e6a928dc043f7f6dfd90a9374a28421d7339
Galuta Dmitrii <dvgaluta@gmail.com>
2022-03-26 00:56:53 +0300

    commit_3

--- mane.py
+++ mane.py
@@ -1,4 +1,5 @@
-#from typing import Callable
+
+import os
 from datetime import datetime
 
 #Написать декоратор - логгер. Он записывает в файл дату и время вызова функции, имя функции, аргументы, с которыми вызвалась и возвращаемое значение.
@@ -12,22 +13,31 @@ def loger_decorator(old_function):
 	def new_function(*args, **kwars):
 		current_datetime = datetime.now()
 		print(current_datetime)
-		print(f'имя функции {old_function.__name__}')
-		print(f'с аргументами {args} и {kwars}')
+		name_function = f'имя функции {old_function.__name__}'
+		print(name_function)
+		arguments_function = f'с аргументами {args}'
+		print(arguments_function)
 		resultat = old_function(*args,**kwars)
 		print(f'результат функции {resultat}')
+		with open('log_file.txt', 'a', encoding="utf-8") as f:
+	    	  path_to_file = os.path.abspath('log_file.txt')
+	    	  f.write(f'{current_datetime} {name_function} {arguments_function} результат функции {resultat} '
+					  f' путь к файлу {path_to_file}\n')
 		return resultat
 	return new_function
 
+# with open('log_file.txt', 'a') as f:
+# 	f.write(f'{some_pat}')
+
 @ loger_decorator
 def multiplier(a, b):
-   return a + b
+    return a + b
 
 if __name__ == '__main__':
-	aa = [1,2,4,5]
-	bb = ['d','c','d','e']
-	x = multiplier(aa, bb)
-	print(x)
+    aa = [1, 2, 4, 5]
+    bb = ['d', 'c', 'd', 'e']
+    x = multiplier(aa, bb)
+    print(x)
 
 
 
